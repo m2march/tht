3bdb9823fa3ba27f84820a4f28de02b5bf498cc7
March <m2.march@gmail.com>
2020-03-03 20:40:17 -0300

    Properly finidng packages

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='tht',
       version='0.1',
       description='Tactus Hypothesis Tracking Module',
       author='Martin "March" Miguel',
       author_email='m2.march@gmail.com',
-      packages=['m2.tht'],
+      packages=find_packages(),
       namespace_packages=['m2'],
       install_requires=[
           'addict',
